1600154e336f8aa05e84067b21550f905e5700b7
dennisgbrown <dennis.g.brown@gmail.com>
2023-01-14 15:42:16 -0600

    very start of a nexus server implementation

---
+++ api/api.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+import random
+import string
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class FileStats(Resource):
+    def get(self):
+        # data = pd.read_csv('users.csv')  # read CSV
+        # data = data.to_dict()  # convert dataframe to dictionary
+        data = {}
+        data['whatever'] = 'stuff ' + (''.join(random.choice(string.ascii_lowercase) for i in range(10)))
+        return data, 200  # return data and 200 OK code
+
+
+api.add_resource(FileStats, '/filestats')  # entry point
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
+
