cf863c17f75ee1ed78ed224a6befcad0a3fba429
Dennis Brown <dennis@denniss-mbp.attlocal.net>
2023-01-20 23:08:01 -0600

    stub out session and data functionality

--- api/api.py
+++ api/api.py
@@ -10,32 +10,72 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-blocks = ''
+
+#============================
+# Break out to another file, later
+
+class SessionControllers:
+    def __init__(self):
+        self.sessionControllers = {}
+
+    def addSessionController(self, sessionController):
+        self.sessionControllers[sessionController.userId] = sessionController
+
+
+class SessionController:
+    def __init__(self, userId):
+        self.userId = userId
+
+
+
+
+
+#============================
+# Also break out to another file, later
+
+class Program:
+    def __init__(self, folder):
+        blocks_file = open(folder + '/basic_blocks')
+        blocks = json.load(blocks_file)
+        blocks_file.close()
+        
+        # print(blocks[0])
+        for i in blocks[0]['basic_blocks']:
+            print(i)
+            # print(blocks[0]['basic_blocks'][i])
+
+
+
+
+#============================
+
+
+
 
 class SyncPortal(Resource):
     def post(self):
         content = request.get_json(force = True)
         print('POSTED: userId =', content['userId'], 'command =', content['command'])
 
+        if (command == 'session_init'):
+            sessionController = SessionController(userId)
+            sessionControllers.addSessionController(sessionController)
+
+
+        # return nonsense for now
         stub = {}
         stub['whatever'] = 'stuff ' + (''.join(random.choice(string.ascii_lowercase) for i in range(10)))
-
         return stub, 200  # return data and 200 OK code
 
 
+program = ''
+sessionControllers = ''
+
 api.add_resource(SyncPortal, '/sync_portal')  # entry point
 
-
 if __name__ == '__main__':
-
-    blocks_file = open('../data/samples/elf_fib_recursive/basic_blocks')
-    blocks = json.load(blocks_file)
-    #print(blocks[0])
-    # for i in blocks[0]['basic_blocks']:
-    #     print(i)
-    #     print(blocks[0]['basic_blocks'][i])
-
-    blocks_file.close()
+    program = Program('../data/samples/elf_fib_recursive')
+    sessionControllers = SessionControllers()
 
     app.run()  # run our Flask app
 
