e9c573b27cefa8252e8a507cecdb0212cdb4c972
dennisgbrown <dennis.g.brown@gmail.com>
2023-03-18 16:47:42 -0500

    now provide asm instructions

--- api/api.py
+++ api/api.py
@@ -35,14 +35,37 @@ class SessionController:
 
 class Program:
     def __init__(self, folder):
-        blocks_file = open(folder + '/basic_blocks')
-        self.blocks = json.load(blocks_file)
-        blocks_file.close()
-        
-        # print(blocks[0])
-        # for i in blocks[0]['basic_blocks']:
-        #     print(i)
-        #     print(blocks[0]['basic_blocks'][i])
+        blocksFile = open(folder + '/basic_blocks')
+        self.blocks = json.load(blocksFile)
+        blocksFile.close()
+        self.blocks = self.blocks[0]['basic_blocks']
+
+        # for i in self.blocks:
+        #     print(f"{i}: {self.blocks[i]['members']}")
+
+        asmFile = open(folder + '/disassembly')                 
+        self.asm = json.load(asmFile)
+        asmFile.close()
+        self.asm = self.asm[0]['instructions']
+
+        # for i in self.asm:
+        #     print(f"{i}: {self.asm[i]['mnemonic']} {self.asm[i]['op_str']}")
+
+        for i in self.blocks.keys():
+            block = self.blocks[i]
+            instructions = {}
+            for j in block['members']:
+                j = str(j)
+                if (j in self.asm.keys()):
+                    # print(f"Block {i} Member {j}: {self.asm[j]['mnemonic']} {self.asm[j]['op_str']}")
+                    instructions[j] = f"{self.asm[j]['mnemonic']} {self.asm[j]['op_str']}"
+            block['insns'] = instructions
+
+        # for i in self.blocks:
+        #     print(f"{i}: {self.blocks[i]}")
+
+        # print(json.dumps(self.blocks))
+
 
     def getBlocksJson(self):
         return json.dumps(self.blocks)
