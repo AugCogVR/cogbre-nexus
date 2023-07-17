19847ee749de51fa33589c204f358315cc00d434
dennisgbrown <dennis.g.brown@gmail.com>
2023-07-16 21:41:38 -0500

    update test messages

--- api/api_test.py
+++ api/api_test.py
@@ -1,12 +1,18 @@
 import requests
 import json
 
+print("\n========================")
+print("TEST: get_compviz_stages fib-func")
+print("========================")
+
 r = requests.post('http://127.0.0.1:5000/sync_portal', json={"userId": "123", "command":"get_compviz_stages fib-func"})
 # print(r.status_code)
 # print(r.json())
 parsed = json.loads(r.json())
 print(json.dumps(parsed, indent = 4))
 
+print("\n========================")
+print("TEST: get_canned_oxide_program elf_fib_recursive")
 print("========================")
 
 r = requests.post('http://127.0.0.1:5000/sync_portal', json={"userId": "123", "command":"get_canned_oxide_program elf_fib_recursive"})
@@ -15,5 +21,5 @@ r = requests.post('http://127.0.0.1:5000/sync_portal', json={"userId": "123", "c
 parsed = json.loads(r.json())
 print(json.dumps(parsed, indent = 4))
 
-print("========================")
+print("\n========================")
 
