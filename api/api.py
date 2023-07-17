b75f0557ed30489b5b4314ad6fc1a585156524d9
dennisgbrown <dennis.g.brown@gmail.com>
2023-07-16 21:32:55 -0500

    import oxide if path specified on command line

--- api/api.py
+++ api/api.py
@@ -6,6 +6,8 @@ import random
 import string
 import json
 import os
+import sys
+import argparse
 from oxide_program import *
 from compviz import *
 from session import *
@@ -13,6 +15,17 @@ from session import *
 
 app = Flask(__name__)
 api = Api(app)
+
+# Import Oxide if Oxide path is given
+parser = argparse.ArgumentParser('cogbre nexus API server')
+parser.add_argument("--oxidepath", type=str, help="Path to active Oxide installation.", required=False)
+args = parser.parse_args()
+useOxide = not(args.oxidepath is None)
+if (useOxide):
+    print(args.oxidepath)
+    sys.path.append(args.oxidepath)
+    from core import oxide as local_oxide
+    print(local_oxide.collection_names())
 
 
 class SyncPortal(Resource):
@@ -32,8 +45,8 @@ class SyncPortal(Resource):
         elif (commandList[0] == "get_session_update"):
             responseString = "session update requested for user " + content["userId"]
 
-        elif (commandList[0] == "get_oxide_program"):
-            oxideProgram = OxideProgram(os.path.join(oxideProgramsLocation, commandList[1]))
+        elif (commandList[0] == "get_canned_oxide_program"):
+            oxideProgram = OxideProgram(os.path.join(cannedOxideProgramsLocation, commandList[1]))
             responseString = oxideProgram.getBlocksJson()
 
         elif (commandList[0] == "get_compviz_stages"):
@@ -44,10 +57,10 @@ class SyncPortal(Resource):
 
 
 sessionControllers = []
-oxideProgramsLocation = os.path.join("data", "samples", "bre")
+cannedOxideProgramsLocation = os.path.join("data", "samples", "bre")
 compVizProgramsLocation = os.path.join("data", "samples", "compviz")
 
-api.add_resource(SyncPortal, "/sync_portal")  # entry point
+api.add_resource(SyncPortal, "/sync_portal")  # the primary/only entry point -- not following API best practices of one resource/entry point per function
 
 if __name__ == "__main__":
     
