6453d379a22c5072ca9c2b9e094e07caf79663b5
dennisgbrown <dennis.g.brown@gmail.com>
2024-11-04 20:59:12 -0600

    moving functionality to React app

--- api/__main__.py
+++ api/__main__.py
@@ -198,10 +198,28 @@ class GUISyncEndpoint(Resource):
     def post(self):
         content = request.get_json(force = True)
         commandList = content["command"]
+        responseString = f"Command not processed: {commandList}"
         print(f"GUI POSTED: command = {commandList}")
-        # print(f"GUI POSTED")
-        response = [{"a":"WHAT"}]
-        return json.dumps(response), 200
+
+        if (commandList[0] == "hello"):
+            response = [{"a":"WHAT"}]
+            return json.dumps(response), 200
+        
+        elif (commandList[0] == "userInfo"):
+            response = [{"a":"WHAT"}]
+            # htmlString = ""
+            # for user in list(userSessions.userSessions.values()):
+            #     if (user.isActive):
+            #         htmlString += f"User: {user.userId}<p>"
+            #         htmlString += user.latestTelemetryString + "<p>"
+            # if (htmlString == ""):
+            #     htmlString += "No active user sessions"
+            # # print(htmlString)
+            # return htmlString
+            return json.dumps(response), 200
+
+        # If we get here, there is an error.
+        return json.dumps(responseString), 500  
 
 
 # Set up web app
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 
 function App() {
 
-  const [data, setData] = useState(null);
+  const [hello, setHello] = useState(null);
   const [error, setError] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
+  useEffect(() => {    
+    var refreshFailCounter = 0;
+    var refreshIntervalId = null;
+  
+    const fetchHello = async () => {
       try {
         const response = await fetch('/gui_sync', {
           method: 'POST', 
@@ -17,32 +21,59 @@ function App() {
           },
           body: JSON.stringify({ command: ['hello'] }), 
         });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
         const result = JSON.parse(await response.json());
-        setData(result[0].a);
+        setHello(result[0].a);
         console.log('Got some junk: ', result);
       } catch (err) {
         setError(err.message);
       }
     };
-    fetchData();
+  
+    const fetchUserInfo = async () => {
+      try {
+        const response = await fetch('/gui_sync', {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json', 
+          },
+          body: JSON.stringify({ command: ['userInfo'] }), 
+        });
+        if (!response.ok) {
+          refreshFailCounter++;
+          setUserInfo(`Error #${refreshFailCounter} fetching data`);
+          console.log(`Error #${refreshFailCounter} fetching data`);
+          if (refreshFailCounter > 10) 
+          {
+              setUserInfo('Too many errors -- ending attempts to fetch data. Reload page to retry.');
+              clearInterval(refreshIntervalId);
+          }
+          throw new Error('Network response was not ok');
+        }
+        refreshFailCounter = 0;
+        const result = JSON.parse(await response.json());
+        setUserInfo(result[0].a);
+        // console.log('Got more junk: ', result);
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+
+    fetchHello();
+
+    refreshIntervalId = setInterval(fetchUserInfo, 500);
+    fetchUserInfo();
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello everyone. Blah. {data}</p>
+        <h1>Generic greeting:</h1>
+        <p>Hello everyone. Blah. {hello}</p>
+        <h1>UserInfo:</h1>
+        <p>{userInfo}</p>
       </header>
     </div>
   );
